0b328cff0f6337498c57d5216eebaffd6bf32a3d
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-10-17 10:35:38 +0200

    Initial version

---
+++ BlazorDragAndDrop/Pages/Index.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlazorDragAndDrop.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorDragAndDrop.Pages
+{
+    public partial class Index : ComponentBase
+    {
+        public List<TodoItem> MyToDoList = new List<TodoItem>()
+        {
+            new TodoItem { Title = "1 Drag this item" },
+            new TodoItem { Title = "2 Add another item" },
+            new TodoItem { Title = "3 Remove this item" }
+        };
+
+        private string newTodo;
+
+        private void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodo))
+            {
+                MyToDoList.Add(new TodoItem { Title = newTodo });
+                newTodo = string.Empty;
+            }
+        }
+
+        private void Save()
+        {
+            Console.WriteLine(string.Join(", ", MyToDoList.Select(x => $"'{x.Title}'")));
+        }
+
+        public void ItemDropped(TodoItem item)
+        {
+            Console.WriteLine("ItemDropped = '{0}'", item.Title);
+
+            StateHasChanged();
+        }
+    }
+}
